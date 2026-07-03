565d896863a9859bf5a22891175fbdeaf70a1700
Yerolu Sherly Leona <sherlyleona.y@gmail.com>
2026-07-03 13:33:14 +0530

    remove incorrect folders

--- OneDrive/autosuggest/server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = 3002;
-app.use(express.static("frontend"));
-app.listen(PORT,function(){
-    console.log("successfully running at http://localhost:"+PORT);
-})
