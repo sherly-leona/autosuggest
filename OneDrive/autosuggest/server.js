712be25af89a29085fee0df2753ef790c92811b0
Yerolu Sherly Leona <sherlyleona.y@gmail.com>
2026-07-03 13:41:40 +0530

    updated port

--- OneDrive/autosuggest/server.js
+++ OneDrive/autosuggest/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = 3002;
 app.use(express.static("frontend"));
 app.listen(PORT,function(){
     console.log("successfully running at http://localhost:"+PORT);
