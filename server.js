f05b98af39a246a8e16f42e077af075eaff38aae
Yerolu Sherly Leona <sherlyleona.y@gmail.com>
2026-07-04 09:56:17 +0530

    randomuser

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3002;
+console.log("backend");
 app.use(express.static("frontend"));
 app.listen(PORT,function(){
     console.log("successfully running at http://localhost:"+PORT);
